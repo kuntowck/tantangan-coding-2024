721224767ab37198b653ff38e324185671112adc
kuntowck <kunto312002@gmail.com>
2024-03-28 12:43:58 +0700

    menggunakan range();

--- 2-counByX.php
+++ 2-counByX.php
@@ -1,3 +1,22 @@
 <?php
-// 1/100
+// 2/100
 // https://www.codewars.com/kata/5513795bd3fafb56c200049e
+
+// function countBy($x, $n)
+// {
+//    $z = [];
+
+//    for ($i = 1; $i <= $n; $i++) {
+//       // array_push($z, $i * $x);
+//       $z[] = $i * $x;
+//    }
+
+//    return $z;
+// }
+
+function countBy($x, $n) {
+   return range($x, $x * $n, $x);
+}
+
+
+echo implode(countBy(2, 5));
